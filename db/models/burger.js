00ffd5e8a58fc4fa8b67b36f4c538ae626935bc1
garret <garretgolledge@gmail.com>
2018-04-23 19:40:44 -0700

    burger.js

---
+++ db/models/burger.js
@@ -0,0 +1,23 @@
+var orm = requrire("../config/orm.js"); 
+
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res); 
+        }); 
+    }, 
+
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res); 
+        }); 
+    }, 
+
+    updateOne: function (objColVals, conditon, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res); 
+        }); 
+    }
+}; 
+
+module.exports = burger; 

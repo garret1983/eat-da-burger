c8449f6d5c9b37c3a377bc461f258ffc70e5cb99
garret <garretgolledge@gmail.com>
2018-04-23 19:11:36 -0700

    connection.js

---
+++ db/config/connection.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3000,
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+}
+
+connection.connect(function(err) {
+    if (err) {
+        console.log("error connecting"); 
+        return; 
+    }
+    console.log("connected as id " + connection.threadId); 
+}); 
+
+module.exports = connection; 
